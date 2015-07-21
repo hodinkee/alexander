75d0bbad1d1b0e90b324f3d0319d9460284957f3
Caleb Davenport <caleb@calebd.me>
2015-07-20 17:00:40 -0700

    Initial commit.

---
+++ Alexander/Alexander.h
@@ -0,0 +1,19 @@
+//
+//  Alexander.h
+//  Alexander
+//
+//  Created by Caleb Davenport on 7/20/15.
+//  Copyright (c) 2015 Hodinkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Alexander.
+FOUNDATION_EXPORT double AlexanderVersionNumber;
+
+//! Project version string for Alexander.
+FOUNDATION_EXPORT const unsigned char AlexanderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Alexander/PublicHeader.h>
+
+
