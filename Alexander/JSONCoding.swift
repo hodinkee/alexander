94118c12aa5ba610c5ced7a7c9c95d337b06558f
Caleb Davenport <caleb@calebd.me>
2015-08-17 10:08:18 -0700

    Work on protocols.
    - Add JSONCoding protocol
    - Make JSONEncodable protocol return a `JSON`

--- Alexander/JSONCoding.swift
+++ Alexander/JSONCoding.swift
@@ -1,22 +1,23 @@
 //
-//  JSONDecodable.swift
+//  JSONCoding.swift
 //  Alexander
 //
-//  Created by Jonathan Baker on 8/6/15.
+//  Created by Caleb Davenport on 8/17/15.
 //  Copyright (c) 2015 Hodinkee. All rights reserved.
 //
 
-import Foundation
+public protocol JSONEncodable {
+    var JSON: Alexander.JSON { get }
+}
 
 public protocol JSONDecodable {
     static func decode(JSON: Alexander.JSON) -> Self?
 }
 
 public extension Alexander.JSON {
-    
-    // MARK: - Functions
-    
     public func decodeArray<T: JSONDecodable>(type: T.Type) -> [T]? {
         return decodeArray(T.decode)
     }
 }
+
+public protocol JSONCoding: JSONEncodable, JSONDecodable {}
