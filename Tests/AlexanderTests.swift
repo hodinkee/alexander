d3d21b2d2f5c84040c342fb953d55a3d64b1c00a
Caleb Davenport <caleb@calebd.me>
2015-10-28 13:48:01 -0700

    Refactor tests configuration.

---
+++ Tests/AlexanderTests.swift
@@ -0,0 +1,14 @@
+//
+//  AlexanderTests.swift
+//  Alexander
+//
+//  Created by Caleb Davenport on 7/20/15.
+//  Copyright (c) 2015 Hodinkee. All rights reserved.
+//
+
+import Alexander
+import XCTest
+
+final class AlexanderTests: XCTestCase {
+
+}
