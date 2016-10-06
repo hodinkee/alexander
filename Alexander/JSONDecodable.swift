79375cd7fd7f9ade2a66854419b9093cc1c327c0
Jonathan Baker <jonathan@jonathanbaker.me>
2016-10-06 17:06:02 -0400

    Add support for decoding an array of decodable.

--- Alexander/JSONDecodable.swift
+++ Alexander/JSONDecodable.swift
@@ -17,11 +17,26 @@ extension JSON {
         return try T.init(json: self)
     }
 
+    public func value<T: JSONDecodable>() throws -> [T] {
+        let array: [AnyObject] = try value()
+        return try array.map({ JSON(value: $0) }).flatMap({ try T.init(json: $0) })
+    }
+
     public func value<T: JSONDecodable>(atIndex index: Int) throws -> T {
         return try value(atIndex: index).value()
     }
 
+    public func value<T: JSONDecodable>(atIndex index: Int) throws -> [T] {
+        let array: [AnyObject] = try value(atIndex: index)
+        return try array.map({ JSON(value: $0) }).flatMap({ try T.init(json: $0) })
+    }
+
     public func value<T: JSONDecodable>(forKey key: String) throws -> T {
         return try value(forKey: key).value()
+    }
+
+    public func value<T: JSONDecodable>(forKey key: String) throws -> [T] {
+        let array: [AnyObject] = try value(forKey: key)
+        return try array.map({ JSON(value: $0) }).flatMap({ try T.init(json: $0) })
     }
 }
--- Tests/JSONDecodableTests.swift
+++ Tests/JSONDecodableTests.swift
@@ -11,51 +11,53 @@ import XCTest
 
 final class JSONDecodableTests: XCTestCase {
 
-    private struct Article: JSONDecodable {
-        var id: Int
-
-        var title: String
-
-        var author: Author
-
-        init(json: JSON) throws {
-            id = try json.value(forKey: "id")
-            title = try json.value(forKey: "title")
-            author = try json.value(forKey: "author")
-        }
-    }
-
     private struct Author: JSONDecodable {
         var name: String
 
         var biography: String?
 
+        var articles: [Article]
+
         init(json: JSON) throws {
             name = try json.value(forKey: "name")
             biography = try? json.value(forKey: "biography")
+            articles = try json.value(forKey: "articles")
         }
     }
 
-    func testArticleDecodable() {
+    private struct Article: JSONDecodable {
+        var id: Int
+
+        var title: String
+
+        init(json: JSON) throws {
+            id = try json.value(forKey: "id")
+            title = try json.value(forKey: "title")
+        }
+    }
+
+    func testAuthorDecodable() {
         let json = JSON(value: [
-            "article": [
-                "id": 1337,
-                "title": "Hello World",
-                "author": [
-                    "name": "John Doe"
+            "author": [
+                "name": "John Doe",
+                "articles": [
+                    ["id": 1, "title": "Computer Science 101"],
+                    ["id": 2, "title": "Computer Science 201"],
+                    ["id": 3, "title": "Computer Science 301"]
                 ]
             ]
         ])
 
         do {
-            let article: Article = try json.value(forKey: "article")
-            XCTAssertEqual(article.id, 1337)
-            XCTAssertEqual(article.title, "Hello World")
-            XCTAssertEqual(article.author.name, "John Doe")
-            XCTAssertNil(article.author.biography)
+            let author: Author = try json.value(forKey: "author")
+            print(author)
+            XCTAssertNil(author.biography)
+            XCTAssertEqual(author.name, "John Doe")
+            XCTAssertEqual(author.articles.count, 3)
+            XCTAssertEqual(author.articles[1].title, "Computer Science 201")
         }
         catch {
-            XCTFail()
+            XCTFail("Error: \(error)")
         }
     }
 }
